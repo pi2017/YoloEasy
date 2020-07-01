7559f4ec7a5f8e0c5c706ec4626ce25ad4c61bfd
Oleksii Savchenko <savskiev@gmail.com>
2020-07-01 12:26:05 +0300

    Add project files

---
+++ yolo3detect.py
@@ -0,0 +1,75 @@
+"""
+YOLO3 OBJECT DETECTION
+Coding by MakeRobot
+Kyiv, 2020
+
+yolov3.weights - for slow detection
+yolov3-tiny.weights - for fast detection
+"""
+
+import cv2 as cv
+import numpy as np
+####### Video capture ###################
+cap = cv.VideoCapture('d:/video/car_video.mp4') # input the pass to test video
+whT = 320
+confThreshold = 0.5
+nmsThreshold = 0.2
+
+####### Load models ###################
+####### Coco names ####################
+
+classesFile = "coco.names"
+classNames = []
+with open(classesFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+print(classNames)
+
+####### Model Files ###################
+modelConfiguration = "yolo/yolov3-tiny.cfg"
+modelWeights = "yolo/yolov3-tiny.weights"
+net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+
+def findObjects(outputs, img):
+    hT, wT, cT = img.shape
+    bbox = []
+    classIds = []
+    confs = []
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                w, h = int(det[2] * wT), int(det[3] * hT)
+                x, y = int((det[0] * wT) - w / 2), int((det[1] * hT) - h / 2)
+                bbox.append([x, y, w, h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+
+    indices = cv.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
+
+    for i in indices:
+        i = i[0]
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        # print(x,y,w,h)
+        cv.rectangle(img, (x, y), (x + w, y + h), (255, 200, 255), 1)
+        cv.putText(img, f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%',
+                   (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 1)
+
+
+while True:
+    success, img = cap.read()
+
+    blob = cv.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+    net.setInput(blob)
+    layersNames = net.getLayerNames()
+    outputNames = [(layersNames[i[0] - 1]) for i in net.getUnconnectedOutLayers()]
+    outputs = net.forward(outputNames)
+    findObjects(outputs, img)
+
+    cv.imshow('YoloDetection', img)
+    cv.waitKey(1)
